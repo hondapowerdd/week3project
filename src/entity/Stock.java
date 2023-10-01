110bf819efd79656e53a78513f87d2e250cb42de
EL5 <linedison3@gmail.com>
2023-09-30 22:45:24 -0400

    added parameter to stock entity

--- src/entity/Stock.java
+++ src/entity/Stock.java
@@ -1,4 +1,15 @@
 package entity;
 
 public class Stock {
+    private String symbol;
+    private String interval;
+    private String exchange;
+    private String mic_code;
+    private String country;
+    private String type;
+    private String outputsize;
+    private String format;
+    private String delimiter;
+    private String apikey;
+    private String prepost;
 }
--- src/use_case/getPrice.java
+++ src/use_case/getPrice.java
@@ -1,4 +1,6 @@
 package use_case;
 
+
+
 public class getPrice {
 }
